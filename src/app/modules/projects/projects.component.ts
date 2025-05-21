8b307550e5ad79823bcf55c5c8b4fd7e7e452fdb
Vinícius Arruda <arrudavini25@gmail.com>
2025-05-21 16:25:19 -0300

    mudança na estilização de algumas páginas

--- src/app/modules/projects/projects.component.ts
+++ src/app/modules/projects/projects.component.ts
@@ -14,6 +14,11 @@ export class ProjectsComponent {
       name: 'RateMaster',
       description: 'Um site destinado a conter produtos e avaliações',
       github: 'https://github.com/AnthonyVinicius/RateMaster'
+    },
+    {
+      name:'Face and Fall Detection',
+      description:'Um algoritmo feito com o propósito de registrar usuários com base em reconhecimento facial  e reconhecer quedas',
+      github: 'https://github.com/vjarruda/detection-of-face-and-fall'
     }
   ];
 }
